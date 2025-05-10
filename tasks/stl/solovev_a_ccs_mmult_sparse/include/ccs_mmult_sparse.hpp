f0e306175686c285f230fe45432f58d9f9d3e3ae
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2025-05-10 23:55:42 +0300

    Update ccs_mmult_sparse.hpp

--- tasks/stl/solovev_a_ccs_mmult_sparse/include/ccs_mmult_sparse.hpp
+++ tasks/stl/solovev_a_ccs_mmult_sparse/include/ccs_mmult_sparse.hpp
@@ -14,6 +14,7 @@
 #include "core/task/include/task.hpp"
 
 namespace solovev_a_matrix_stl {
+
 struct MatrixInCcsSparse {
   std::vector<std::complex<double>> val;
   std::vector<int> row;
@@ -23,25 +24,21 @@ struct MatrixInCcsSparse {
   int c_n;
   int n_z;
 
-  MatrixInCcsSparse(int r_nn = 0, int c_nn = 0, int n_zz = 0) {
-    c_n = c_nn;
-    r_n = r_nn;
-    n_z = n_zz;
-    row.resize(n_z);
-    col_p.resize(c_n + 1);
-    val.resize(n_z);
-  }
+  MatrixInCcsSparse(int r_nn = 0, int c_nn = 0, int n_zz = 0)
+      : r_n(r_nn), c_n(c_nn), n_z(n_zz), val(n_zz), row(n_zz), col_p(c_nn + 1) {}
 };
 
 class SeqMatMultCcs : public ppc::core::Task {
  public:
-  explicit SeqMatMultCcs(std::shared_ptr<ppc::core::TaskData> task_data) : Task(std::move(task_data)) {}
+  explicit SeqMatMultCcs(std::shared_ptr<ppc::core::TaskData> task_data)
+      : Task(std::move(task_data)) {}
+
   bool PreProcessingImpl() override;
   bool ValidationImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
-  static void worker_loop(solovev_a_matrix_stl::SeqMatMultCcs* self);
+  static void worker_loop(SeqMatMultCcs* self);
 
  private:
   MatrixInCcsSparse *M1_, *M2_, *M3_;
@@ -50,10 +47,13 @@ class SeqMatMultCcs : public ppc::core::Task {
   std::once_flag init_flag_;
   std::mutex mtx_;
   std::condition_variable cv_;
-  std::atomic<int> next_col_;
+  std::atomic<int> next_col_{0};
+  std::atomic<int> completed_{0};
   int phase_ = 0;
-  int r_n_, c_n_;
+  int r_n_ = 0, c_n_ = 0;
+  bool terminate_ = false;
 
   std::vector<int> counts_;
 };
+
 }  // namespace solovev_a_matrix_stl
